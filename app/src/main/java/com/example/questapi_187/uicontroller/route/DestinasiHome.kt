640efdddf3d7fa8849f8b5fe6686c0c4b215a8f9
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2025-12-19 19:23:03 +0700

    Menambahkan destinasi navigasi untuk halaman home

---
+++ app/src/main/java/com/example/questapi_187/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.questapi_187.uicontroller.route
+
+import com.example.questapi_187.R
+
+object DestinasiHome: DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = R.string.nama
+}
