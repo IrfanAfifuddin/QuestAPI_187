468df2ddc9bd2d4636699cb2278a10dd323385eb
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2025-12-26 18:01:07 +0700

    menambahkan init editviewmodel pada PenyediaViewModel.kt

--- app/src/main/java/com/example/questapi_187/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/questapi_187/viewmodel/provider/PenyediaViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.questapi_187.viewmodel.provider
 
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
@@ -7,6 +8,7 @@ import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.questapi_187.repositori.AplikasiDataSiswa
 import com.example.questapi_187.viewmodel.EntryViewModel
 import com.example.questapi_187.viewmodel.HomeViewModel
+import com.example.questapi_187.viewmodel.EditViewModel
 
 fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
@@ -25,6 +27,12 @@ object PenyediaViewModel {
             EntryViewModel(
                 aplikasiDataSiswa().container
                     .repositoryDataSiswa
+            )
+        }
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoryDataSiswa
             )
         }
     }
