f0f19eca264dee04f4c84598dd234db0ddfb806c
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2025-12-26 18:06:08 +0700

    meanmbahkan value pada DestinasiEdit

--- app/src/main/java/com/example/questapi_187/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/questapi_187/uicontroller/route/DestinasiEdit.kt
@@ -1,4 +1,10 @@
 package com.example.questapi_187.uicontroller.route
 
-class DestinasiEdit {
+import com.example.questapi_187.R
+
+object DestinasiEdit : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    val itemIdArg = "itemId"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
